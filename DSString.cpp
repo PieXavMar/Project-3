f6817d4b4c94d1a67394ad71bda4e46f22446576
Pierre Martinez <pxmartinez@smu.edu>
2024-12-02 17:31:55 -0600

    more constructors

--- DSString.cpp
+++ DSString.cpp
@@ -6,13 +6,27 @@ DSString::DSString(){
     data[0] = '\0';
     length = 0;
 }
+// destructor
+DSString::~DSString() {
+    delete[] data;
+}
 //c-string constructor
 DSString::DSString(const char *str){
     int i;
-    length = setlength(stl);
+    length = setlength(str);
     data = new char[length + 1];
     data[length] = '\0';
-    for(i=0; i < length; 1++){
-        data[i] = stl.data[i];
+    for(i=0; i < length; i++){
+        data[i] = str.data[i];
+    }
+}
+//copy constructor
+DSString::DSString(const DSString &str){
+    length = str.length;
+    data = new char[length + 1];
+    //null terminate
+    data[length] = '\0';
+    for(int i=0; i<length; i++){
+        data[i] = str.data[i];
     }
 }
